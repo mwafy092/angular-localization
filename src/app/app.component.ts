d99651d737b2ffa32af29fc4bb16c163a3433f20
MustafaMwafy <mwafy092@gmail.com>
2023-07-08 01:52:04 +0300

    add buttons for changing the language

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'localization-app';
+  languageList = [
+    { code: 'en', label: 'English' },
+    { code: 'ar', label: 'Arabic' },
+  ];
 }
